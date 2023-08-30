b71dc180236168e6bb972fde5782fb0d58a490d6
Francisco Timez <franciscotimez@gmail.com>
2023-08-30 10:18:50 -0300

    solucion

--- src/clean-code/07-tarea.ts
+++ src/clean-code/07-tarea.ts
@@ -1,43 +1,49 @@
-(()=>{
+(() => {
 
     //* Aplicar el principio de responsabilidad única
     //* Priorizar la composición frente a la herencia
 
-    type HtmlType = 'input'|'select'|'textarea'|'radio';
-   
+    type HtmlType = 'input' | 'select' | 'textarea' | 'radio';
+
     class HtmlElement {
         constructor(
             public id: string,
             public type: HtmlType,
-        ) {}
+        ) { }
     }
 
-    class InputAttributes extends HtmlElement {
+    class InputAttributes {
         constructor(
             public value: string,
             public placeholder: string,
-            id: string,
-        ) {
-            super(id, 'input');
-        }
+        ) { }
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
-        }
+    class InputEvents {
+        constructor() { }
 
-        setFocus() {};
-        getValue() {};
-        isActive() {};
-        removeValue() {};
+        setFocus() { };
+        getValue() { };
+        isActive() { };
+        removeValue() { };
     }
 
 
+    class InputElement {
+        public html: HtmlElement;
+        public attributes: InputAttributes;
+        public events: InputEvents;
+
+        constructor(value: string, placeholder: string, id: string) {
+            this.html = new HtmlElement(id, 'input');
+            this.attributes = new InputAttributes(value, placeholder);
+            this.events = new InputEvents();
+        }
+    }
     //? Idea para la nueva clase InputElement
 
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
+    const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
 
     console.log({ nameField });
 
-})()
+})();
